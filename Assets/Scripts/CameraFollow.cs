e145195c84bc917ccbfc7abd699da61c679cd1b7
CodeNinjasEtobicoke <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2023-12-22 18:06:16 -0500

    joshua

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+
     [Header("Target Object")]
     public Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +17,9 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(target.position.y > transform.position.y)
-        {
-            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
-        }
+        if (target.position.y > transform.position.y)
+        { 
+
+    transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
 }
